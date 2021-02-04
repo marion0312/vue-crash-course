85e65d72d736bb4c369b30dbcf6903c72f79123e
Service Unit <Marion Balba>
2021-02-04 22:58:19 +0800

    Finished AddTodo function

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
 import Header from './components/layouts/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'app',
   components: {
     Header,
-    Todos
+    Todos,
+    AddTodo
   },
   data(){
     return {
@@ -40,11 +43,25 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
 </script>
 
 <style>
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
 
+  .btn:hover {
+    background: #666;
+  }
 </style>
